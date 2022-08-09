58636e61eecd098b5143d421cd90e25248db69ca
romeu <passadore.neto@outlook.com>
2022-08-09 15:46:31 -0300

    melhoria no css 1.2

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import MovieRow from './components/MovieRow';
-import'./App.css'
+import './App.css'
 import Tmdb from './Tmdb';
+import FaeturedMovie from './components/FaeturedMovie'
 
 export default () => {
   const [movieList, setMovieList] = useState([]);
+  const [faeturedData, setFaeturedData] = useState(null); 
 
-  useEffect(()=>{
-    const loadAll = async () =>
-    {
+  useEffect(() => {
+    const loadAll = async () => {
       // pegando a lista total
       let list = await Tmdb.getHomeList();
-     
-   
+
+
       setMovieList(list);
-      
+
     }
     loadAll();
-  },[]);
-  return(
+  }, []);
+  return (
     <div className='page'>
+
+      {FaeturedMovie && 
+          <FaeturedMovie items={faeturedData} /> 
+
+      }
+
+
       <section className='lists'>
-        {movieList.map((item,key)=>(
-         <MovieRow key={key} title={item.title} items= {item.items} />
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
         ))}
 
       </section>
-     
+
     </div>
   );
-  
+
 }
---
+++ src/components/FaeturedMovie.js
@@ -0,0 +1,10 @@
+import react from 'react';
+import './FaeturedMovie.css';
+export default () => {
+    return (
+        <div>
+            FILME EM DESTAQUE
+        </div>
+    );
+
+}
