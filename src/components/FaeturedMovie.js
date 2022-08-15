49e69a13a9d1e837abfb5ce26f03fe332bf7ba15
romeu <passadore.neto@outlook.com>
2022-08-15 14:48:02 -0300

    components featured adds btn

--- src/components/FaeturedMovie.js
+++ src/components/FaeturedMovie.js
@@ -3,6 +3,12 @@ import './FaeturedMovie.css';
 export default ({ item }) => {
     console.log(item);
 
+    let firstDate = new Date(item.first_air_date)
+    let genres  = [];
+    for (let i in item.genres) {
+        genres.push(item.genres[i].name);
+    }
+
     return (
         <section className="featured" style={{
             backgroundSize: 'cover',
@@ -15,11 +21,16 @@ export default ({ item }) => {
                     <div className="featured--name">{item.name}</div>
                     <div className="featured--info">
                         <div className="featured--points">{item.vote_average} pontos</div>
-                        <div className="featured--year">2999</div>
+                        <div className="featured--year">{firstDate.getFullYear()}</div>
                         <div className="featured--seasons">{item.number_of_seasons} tenporada{item.number_of_seasons !== 1 ?'s':''}</div>
                     </div>
                     <div className="featured--description">{item.overview}</div>
-                    <div className="featured--"></div>
+                    <div className="featured--buttons">
+                        <a href={`/watch/${item.id}`}>► Assistir</a>
+                        < a href={`/list/add/${item.id}`} >+ Minha Lista</a>
+
+                    </div>
+                    <div className="featured--genres"><strong>Gêneros:</strong>...{genres.join(', ')}</div>
 
                 </div>
 
