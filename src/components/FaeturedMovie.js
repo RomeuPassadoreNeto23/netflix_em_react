7e332bcc819eea02381fc17120a0d60d5239a95f
romeu <passadore.neto@outlook.com>
2022-08-16 15:26:24 -0300

    components FaeturedMovie finalizado

--- src/components/FaeturedMovie.js
+++ src/components/FaeturedMovie.js
@@ -18,7 +18,7 @@ export default ({ item }) => {
         }}>
             <div className="featured--vetical">
                 <div className="featured--horizontal">
-                    <div className="featured--name">{item.name}</div>
+                    <div className="featured--name">{item.original_name}</div>
                     <div className="featured--info">
                         <div className="featured--points">{item.vote_average} pontos</div>
                         <div className="featured--year">{firstDate.getFullYear()}</div>
