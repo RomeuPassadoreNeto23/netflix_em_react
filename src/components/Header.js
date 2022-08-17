3907ba238c2483b8e7476ef4e57922c623de005c
romeu <passadore.neto@outlook.com>
2022-08-17 16:45:26 -0300

    css do components Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,17 @@ import './Header.css';
 
 export default () => {
     return (
-     <header>...</header>
+     <header className="black">
+        <div className="header--logo">
+            <a href="/">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png" alt="Netflix"></img>
+            </a>
+        </div>
+        <div className="header--user">
+                <a href="/">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="usuario"></img>
+                </a>
+            </div>
+     </header>
     );
 }
