4b419c1d7efe50f189d691a933d770e509d1c238
romeu <passadore.neto@outlook.com>
2022-08-25 17:20:02 -0300

    icons da navegação de filme e css

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -1,13 +1,32 @@
 import React, { useEffect, useState } from "react";
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+
+
+
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import './MovieRow.css';
 
 export default ({ title, items }) => {
+    const handlLeftArrow = () => {
+
+    }
+    const handlRightArrow = () => {
+
+    }
     console.log(items)
     return (
         <div className="MovieRow" >
             <h2>{title}</h2>
+            <div className="MovieRow--left">
+                <NavigateBeforeIcon style={{fontSize: 50}} onclick={handlLeftArrow}/>
+            </div>
+            <div className="MovieRow--right">
+                <NavigateNextIcon style={{fontSize: 50}} onclick={handlRightArrow}/>
+            </div>
             <div className="movieRow--listarea">
-                <div className="movieRow--list">
+                <div className="movieRow--list" style={{
+                     marginLeft: 0
+                   }}>
                     {items.results?.length > 0 && items.results.map((item, key) => (
                         <div key={key} className="movieRow--Item">
                             <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} />
