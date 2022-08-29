b03fbba9e9a880ce2bfdddd478f001a3feae425f
Romeu <passadore.neto@outlook.com>
2022-08-29 08:19:56 -0300

    navegação da lista filmes

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -6,15 +6,23 @@ import './MovieRow.css';
 export default ({ title, items }) => {
     const [scrollX, setScrollX] = useState(-400);
     const handlLeftArrow = () => {
-        let  x = scrollX + 150;
-        
+        let x = scrollX + Math.round(window.innerWidth / 2);
+        if (x > 0) {
+            x = 0;
+        }
 
-      
+
         setScrollX(x)
 
 
     }
     const handlRightArrow = () => {
+        let x = scrollX - Math.round(window.innerWidth / 2);
+        let  listM = items.results.length * 150;
+        if((window.innerWidth - listM) > x){
+            x = (window.innerWidth - listM) - 60;
+        }
+        setScrollX(x)
 
     }
     console.log(items)
@@ -22,16 +30,16 @@ export default ({ title, items }) => {
         <div className="MovieRow" >
             <h2>{title}</h2>
             <div className="MovieRow--left" onClick={handlLeftArrow}>
-                <NavigateBeforeIcon style={{fontSize: 50}} />
+                <NavigateBeforeIcon style={{ fontSize: 50 }} />
             </div>
             <div className="MovieRow--right" onClick={handlRightArrow}>
-                <NavigateNextIcon style={{fontSize: 50}} />
+                <NavigateNextIcon style={{ fontSize: 50 }} />
             </div>
             <div className="movieRow--listarea">
                 <div className="movieRow--list" style={{
-                     marginLeft: scrollX,
-                     width: items.results.length * 150
-                   }}>
+                    marginLeft: scrollX,
+                    width: items.results.length * 150
+                }}>
                     {items.results?.length > 0 && items.results.map((item, key) => (
                         <div key={key} className="movieRow--Item">
                             <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} />
